c8000372bc041caf4a9ff99d354e34c41023a96a
pal711 <contactsouvik7@gmail.com>
2019-04-21 12:16:20 +0530

    smarter version

--- codes/any_all.py
+++ codes/any_all.py
@@ -1,24 +1,9 @@
 #/bin/python3
 
-def chk_palindrome(string):
-    strt = 0
-    end = len(string) - 1
-    palindrm = True
-    while strt <= end:
-        if string[strt] != string[end]:
-            palindrm = False
-            break
-        strt += 1
-        end -= 1
-    
-    return palindrm
-
 def func():
     input()
-    numbers = input().split()
-    pos = all(['-' not in x for x in numbers])
-    palindrm = any([chk_palindrome(x) for x in numbers])
-    print(pos and palindrm)
+    str_ = input().split()
+    print(all(['-' not in x for x in str_]) and any( [ x == x[-1::-1] for x in str_]))
 
 if __name__ == '__main__':
     func()
